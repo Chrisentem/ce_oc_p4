c241d62ef0cee4b4f045d450a85374610a929dc6
Unknown <chrisentem@gmail.com>
2018-09-23 18:48:16 +0200

    Adding constructor and properties

--- src/AppBundle/Entity/Purchase.php
+++ src/AppBundle/Entity/Purchase.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
@@ -13,6 +14,23 @@ use Symfony\Component\Validator\Constraints as Assert;
  */
 class Purchase
 {
+
+    const STATUS_0 = "initialized";
+    const STATUS_1 = "confirmed";
+    const STATUS_2 = "paid";
+    const STATUS_3 = "sent";
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+      $this->date = new \Datetime();
+      $this->tickets   = new ArrayCollection();
+      $this->email = "";
+      $this->status = self::STATUS_0;
+    }
+
     /**
      * @var int
      *
@@ -44,6 +62,36 @@ class Purchase
      * @ORM\Column(name="status", type="string", length=255)
      */
     private $status;
+
+    /**
+     * @var \Date
+     *
+     * @ORM\Column(name="date_visit", type="date")
+     * @Assert\Date()
+     */
+    private $dateOfVisit;
+
+    /**
+     * 
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\EntryTicket", mappedBy="Purchase")
+     * @ORM\JoinColumn(nullable=false)
+     * @Assert\Valid()
+     */
+    private $tickets;
+
+    /**
+     * @var int
+     * @Assert\Range(
+     * min = 1,
+     * max = 6,
+     * )
+     */
+    private $numberOfTickets;
+
+    /**
+     * @var string
+     */
+    private $visitType;
 
 
     /**
@@ -127,5 +175,98 @@ class Purchase
     {
         return $this->status;
     }
-}
 
+    /**
+     * Set dateOfVisit
+     *
+     * @param \Date $dateOfVisit
+     *
+     * @return Purchase
+     */
+    public function setDateOfVisit($dateOfVisit)
+    {
+        $this->dateOfVisit = $dateOfVisit;
+
+        return $this;
+    }
+
+    /**
+     * Get dateOfVisit
+     *
+     * @return \Date
+     */
+    public function getDateOfVisit()
+    {
+        return $this->dateOfVisit;
+    }
+
+    /**
+     * Add ticket
+     *
+     * @param \AppBundle\Entity\EntryTicket $ticket
+     *
+     * @return Purchase
+     */
+    public function addTicket(\AppBundle\Entity\EntryTicket $ticket)
+    {
+        $this->tickets[] = $ticket;
+
+        return $this;
+    }
+
+    /**
+     * Remove ticket
+     *
+     * @param \AppBundle\Entity\EntryTicket $ticket
+     */
+    public function removeTicket(\AppBundle\Entity\EntryTicket $ticket)
+    {
+        $this->tickets->removeElement($ticket);
+    }
+
+    /**
+     * Get tickets
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getTickets()
+    {
+        return $this->tickets;
+    }
+
+    /**
+     * Get visitType
+     */
+    public function getVisitType()
+    {
+        return $this->visitType;
+    }
+
+    /**
+     * Set visitType
+     */
+    public function setVisitType($visitType)
+    {
+        $this->visitType = $visitType;
+
+        return $this;
+    }
+
+    /**
+     * Get numberOfTickets
+     */
+    public function getnumberOfTickets()
+    {
+        return $this->numberOfTickets;
+    }
+
+    /**
+     * Set numberOfTickets
+     */
+    public function setnumberOfTickets($numberOfTickets)
+    {
+        $this->numberOfTickets = $numberOfTickets;
+
+        return $this;
+    }
+}
