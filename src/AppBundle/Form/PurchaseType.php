b6eb48d7dd88fb609c2da65b6fca56cc90c078a7
Unknown <chrisentem@gmail.com>
2018-10-22 22:12:04 +0200

    Indentation upgrade

--- src/AppBundle/Form/PurchaseType.php
+++ src/AppBundle/Form/PurchaseType.php
@@ -17,34 +17,34 @@ class PurchaseType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('dateOfVisit', DateType::class, array(
-                'label' => 'Date of visit',
-                'widget' => 'choice',
-                'years' => range(date('Y'), date('Y')+1),
+            ->add('dateOfVisit', DateType::class, array(
+                    'label' => 'Date of visit',
+                    'widget' => 'choice',
+                    'years' => range(date('Y'), date('Y')+1),
+                )
             )
-        )
-        ->add('ticketType', ChoiceType::class, array(
-            'label' => 'Ticket Type',
-            'choices'  => array(
-                'full-day' => 0,
-                'half-day' => 1,
-            ))
-        )
-        ->add('numberOfTickets', ChoiceType::class, array(
-            'label' => 'Qty',
-            'required' => true,
-            'choices' => array_combine(range(1,Purchase::MAX_PURCHASE_TICKETS),range(1,Purchase::MAX_PURCHASE_TICKETS)),
+            ->add('ticketType', ChoiceType::class, array(
+                    'label' => 'Ticket Type',
+                    'choices'  => array(
+                        'full-day' => 0,
+                        'half-day' => 1,
+                    ))
             )
-        );
+            ->add('numberOfTickets', ChoiceType::class, array(
+                    'label' => 'Qty',
+                    'required' => true,
+                    'choices' => array_combine(range(1,Purchase::MAX_PURCHASE_TICKETS),range(1,Purchase::MAX_PURCHASE_TICKETS)),
+                )
+            );
     }
-    
+
     /**
      * {@inheritdoc}
      */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => Purchase::class,            
+            'data_class' => Purchase::class,
             'validation_groups' => array('step1'),
         ));
     }
